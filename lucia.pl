47639a34e42a9d7a79345868a9be1c5378861782
Miguel Expósito <exposito_m@cantabria.es>
2017-04-24 01:16:03 +0200

    Comprobados ciertos requisitos y añadidos nuevos contadores

--- lucia.pl
+++ lucia.pl
@@ -8,12 +8,13 @@ estado(jugado).
 
 % posibles transiciones entre estados
 transicion('despertar', 'despertarla', dormida, despierta).
-transicion('banyar', 'dejarla dormir un poco más', dormida, despierta).
-transicion('hacer_pis', 'banyarla', despierta, banyada).
-transicion('desayunar', 'llevarla a hacer pis', despierta, hecho_pis).
+transicion('dejar_mas', 'dejarla dormir un poco más', dormida, remoloneando).
+transicion('despertar', 'despertarla', remoloneando, despierta).
+transicion('bañar', 'bañarla', despierta, bañada).
+transicion('hacer_pis', 'llevarla a hacer pis', despierta, hecho_pis).
 transicion('desayunar', 'darle el desayuno', despierta, desayunada).
-transicion('jugar_salon', 'dejarla jugar en el salon', despierta, jugado).
-transicion('salir', 'salir', Estado, fin).
+transicion('jugar_salon', 'dejarla jugar un rato en el salon', despierta, jugado).
+transicion('salir', 'salir', Estado, 'triste porque te vas').
 
 despertar(dormida, despierta).
 banyar(despierta, banyada).
@@ -22,76 +23,102 @@ desayunar(despierta, desayunada).
 jugar_salon(despierta, jugado).
 
 % estado inicial
-:-dynamic actual/1, sueño/1.
-sueño(0). 
+:-dynamic actual/1, contador/2.
 actual(dormida).
 
+
+
 inicio :-
-  cambiar(dormida),
+  A is random(100),
+  assert(contador(humor, A)),
+  B is random(100),
+  assert(contador(hambre, B)),
+  assert(contador(sueño,0)),
+ 
   lucia.
 
 lucia :-
+  cambiar(dormida),
   write('Bienvenido a L.U.C.I.A'),
   nl,
   actual(Estado),
-  que_hago(Estado),
+
+  que_hago(Estado, Estado),
   control_principal(Estado).
 
 
-control_principal(Estado) :-
-  condicion_fin(Estado).
-control_principal(Estado) :-
+control_principal(EstadoAnterior) :-
+  condicion_fin(EstadoAnterior).
+control_principal(EstadoAnterior) :-
   repeat,
   write('> '),
   read(X),
-  ir(X),
+  hacer(X),
   actual(NuevoEstado),
-  que_hago(NuevoEstado),
+  que_hago(EstadoAnterior, NuevoEstado),
   control_principal(NuevoEstado).
 
 % condicion de fin
 
 condicion_fin(Estado) :-
-  actual(Estado) = actual(hasta_manyana),
+  actual(Estado) = actual(hasta_mañana),
   write('El juego ha terminado').
 condicion_fin(Estado) :-
-  actual(Estado) = actual(fin),
+  actual(Estado) = actual('triste porque te vas'),
   write('has abandonado el juego').
 
-sucesos(Estado) :-
+sucesos(EstadoAnterior, Estado) :-
 	actual(Estado) = actual(despierta),
-	writeln('Que tal ha dormido mi niña?'),
+	EstadoAnterior = dormida,
+	writeln('papá: Que tal ha dormido mi niña?'),
 	random_between(0, 1, R),
 	cambia_sueño(R).
-sucesos(Estado) :-
+sucesos(EstadoAnterior, Estado) :-
+	actual(Estado) = actual(despierta),
+	EstadoAnterior = remoloneando,
+	writeln('papá: Que tal ha dormido mi niña?'),
+	writeln('nena: Muy bien, papá!!'),
+	writeln('papá: Corre, que vamos tarde...').
+sucesos(EstadoAnterior, Estado) :-
 	true.
 
 cambia_sueño(R) :-
 	R == 0,
-	writeln('Déjameeeeee'),
-	retract(sueño(S)),
+	writeln('nena: Déjameeeeee'),
+	retract(contador(sueño, S)),
 	NuevoSueño is S + 10,
-	asserta(sueño(NuevoSueño)).
+	asserta(contador(sueño,NuevoSueño)).
 cambia_sueño(R) :-
 	R == 1,
-	writeln('Muy bien, papá!!').
+	writeln('nena: Muy bien, papá!!').
 
 % muestra las transiciones posibles desde un estado dado
 listar_transiciones(Estado) :-
 	
   transicion(Accion, Descripcion, Estado, EstadoDestino),
   tab(2),
-  write(Descripcion),
-  write(': Escriba '),
-  write(Accion),
+  write('- '), write(Accion), write(': '), write(Descripcion),
   nl,
   fail.
 listar_transiciones(_).
 
-puedo_ir(Estado):-
+% mostrar indicadores de la niña
+listar_indicadores :-
+  contador(humor,A),
+  contador(hambre,B),
+  contador(sueño,C),
+  tab(2),
+  write('- Humor = '), write(A), nl,
+  tab(2),
+  write('- Hambre = '), write(B), nl,
+  tab(2),
+  write('- Sueño = '), write(C), nl.
+
+
+puedo_hacer(Estado):-
   actual(EstadoActual),
   transicion(Accion, Descripcion, EstadoActual, EstadoDestino).
-puedo_ir(EstadoDestino):-
+puedo_hacer(EstadoDestino):-
   writeln('Ahora no toca hacer eso.'),
   fail.
   
@@ -99,16 +126,15 @@ cambiar(Estado) :-
   retract(actual(X)),
   asserta(actual(Estado)).
     
-ir(Accion):-
+hacer(Accion):-
 	transicion(Accion, Descripcion, EstadoActual, Estado),
-	puedo_ir(Estado),
+	puedo_hacer(Estado),
 	cambiar(Estado).
 
 % indica en que estado se encuentra la ninya y que se puede hacer
-que_hago(Estado) :-
-  write('Lucia esta '), write(Estado), nl,
-  sucesos(Estado),
-  sueño(S),
-  write('Sueño = '), write(S), nl,
-  writeln('Que hacemos?:'),
+que_hago(EstadoAnterior, Estado) :-
+  write('Lucía está '), write(Estado), nl,
+  sucesos(EstadoAnterior, Estado),
+  listar_indicadores,
+  writeln('¿Qué hacemos ahora? (introduzca un comando seguido de punto . )'),
   listar_transiciones(Estado).
--- memoria.md
+++ memoria.md
@@ -4,21 +4,20 @@
 
 Por cada uno de los miembros del grupo de trabajo en el sistema se habrán de definir:
 
-- 2 o más constantes: una constante puede ser un átomo o un número. En el programa actual, hay definidas varias constantes: dormida, despierta, hecho_pis, etc. 
-- 2 o más hechos con variables:
- y 2 o más sin variables: 
-- 5 o más predicados;
+- 2 o más constantes: una constante puede ser un átomo o un número. En el programa actual, hay definidas varias constantes: `dormida`, `despierta`, `hecho_pis`, etc. 
+- 2 o más hechos con variables: `transicion('salir', 'salir', Estado, fin).`, TODO
+- 2 o más hechos sin variables: `transicion('despertar', 'despertarla', dormida, despierta).`, `transicion('banyar', 'dejarla dormir un poco más', dormida, despierta).`
+- 5 o más predicados: por ejemplo, `transicion`, `sucesos`, `hacer`, `que_hago`, `condicion_fin`.
 
-y deberán verificarse las siguientes condiciones:
-al menos uno de los predicados habrá de definirse mediante 2 o más reglas,
-al menos uno de los predicados deberá tener 2 o más argumentos,
-2 o más reglas deberán constar de 2 o más antecedentes y
-la satisfacción de 2 o más objetivos habrá de requerir el encadenamiento de 2 o más reglas.
-2. El sistema deberá contener además:
--
--
--
-Al menos un ejemplo de uso de uno de los operadores aritméticos o relacionales predefinidos.
-Al menos un ejemplo de recursividad.
-Al menos un ejemplo de uso de los predicados de inserción y borrado de hechos de la Base de
-Hechos.
+Y deberán verificarse las siguientes condiciones:
+
+- al menos uno de los predicados habrá de definirse mediante 2 o más reglas: por ejemplo, el predicado `control_principal(Estado)`.
+- al menos uno de los predicados deberá tener 2 o más argumentos: TODO
+- 2 o más reglas deberán constar de 2 o más antecedentes: por ejemplo, la regla cuya cabeza es el predicado `lucia` o la regla cuya cabeza es el predicado `cambia_sueño`
+- la satisfacción de 2 o más objetivos habrá de requerir el encadenamiento de 2 o más reglas: TODO
+
+El sistema deberá contener además:
+
+- Al menos un ejemplo de uso de uno de los operadores aritméticos o relacionales predefinidos: por ejemplo, en el predicado `cambia_sueño` se utiliza la suma.
+- Al menos un ejemplo de recursividad: por ejemplo, el predicado `control_principal`, que se llama a sí mismo en cada estado o fase del programa hasta que llega al estado final en el que termina el mismo.
+- Al menos un ejemplo de uso de los predicados de inserción y borrado de hechos de la Base de Hechos: por ejemplo, los contadores de puntuaciones (`humor`, `sueño`, etc.). Para actualizarse, primero se retractan y luego vuelven a añadirse como hechos pero con distintos valores.
